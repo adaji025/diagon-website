b4f458889b4c0af3b98cac1575323b46ad5b9eb6
Adaji Mukhtar <alfadaji@gmail.com>
2023-07-12 06:07:54 +0100

    separated timeline css

--- src/components/Timeline/PointsTimeline.tsx
+++ src/components/Timeline/PointsTimeline.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import styles from "../../styles/Timeline.module.css";
+import styles from "../../styles/PointsTimeline.module.css";
 
 const PointsTimeline = () => {
   const [activetimeline, setActiveTime] = React.useState(10);
@@ -17,16 +17,14 @@ const PointsTimeline = () => {
   return (
     <div className={`text-white ${styles.timeline_container}`}>
       {timelineData.map((data, index) => (
-        <div className={`flex ${styles.timeline_item_content} `}>
-          <div
-            className={`py-3 pl-10 pr-5 w-[80%] ${
-              index === 0 ? styles.timeline_item_2 : ""
-            }`}
-            key={index}
-          >
-            <p>{data}</p>
-            <span className={`${index === 0 ? styles.points_circle  : ""}`} />
-          </div>
+        <div
+          className={`py-3 pl-10 pr-5 w-[80%] ${
+            index === 0 ? styles.timeline_item : ""
+          }`}
+          key={index}
+        >
+          <p>{data}</p>
+          <span className={`${index === 0 ? styles.points_circle : ""}`} />
         </div>
       ))}
     </div>
