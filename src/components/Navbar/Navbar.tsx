4db9d16baa19e0a0d2514a980227a862c2ff689c
Adaji Mukhtar <alfadaji@gmail.com>
2023-07-15 17:06:27 +0100

    made amend to the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,13 +14,13 @@ const Navbar = () => {
       <div className="bg-black backdrop-blur-[30px] fixed w-full z-50">
         <div className="max-w-[1200px] mx-auto px-4 md:px-6 flex justify-between h-[100px] items-center">
           <div className="flex items-center gap-10 lg:gap-16">
-            <div>
+            <Link href="/">
               <img src="/svgs/diagon-logo.svg" alt="diagon studio" />
-            </div>
+            </Link>
 
             <div className="hidden md:flex gap-5 lg:gap-8 text-white font-medium text-base lg:text-lg">
-              <Link href="https://whitepaper.diagon.io/" className="cursor-pointer">Whitepaper</Link>
-              <Link href="/leadersboard" target="_blank" className="cursor-pointer">Leaderboard</Link>
+              <Link href="https://whitepaper.diagon.io/"  target="_blank" className="cursor-pointer">Whitepaper</Link>
+              <Link href="/leadersboard" className="cursor-pointer">Leaderboard</Link>
               <Link href="https://www.pointstopay.io/" target="_blank" className="cursor-pointer">Shop & Refill</Link>
             </div>
           </div>
