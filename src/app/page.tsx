fee50baab84b04bef6bab6a2451b0e28aad41f83
Adaji Mukhtar <alfadaji@gmail.com>
2023-07-22 06:53:35 +0100

    replicated timeline scroll animation on points

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-"use client";
-
-import { useState } from "react";
 import Carousel from "@/components/Carousel/Carousel";
 import styles from "../styles/Home.module.css";
 import { Arrow } from "@/components/Svg/Svg";
 import Faq from "@/components/Faq/Faq";
-import PointsTimeline from "@/components/Timeline/PointsTimeline";
-import CasualTimeline from "@/components/Timeline/CasualTimeline";
+import PointsComponent from "@/components/PointsComponent";
+import CasualComponent from "@/components/CasualComponent";
 
 export default function Home() {
-  const [backgroundDivIndex, setBackgroundDivIndex] = useState<number | null>(
-    null
-  );
   return (
     <div className="bg-black">
       {/* hero */}
@@ -58,64 +52,10 @@ export default function Home() {
       </div>
 
       {/* Casual description */}
-      <div className="bg-black py-5">
-        <div className={`py-20 ${styles.exp}`}>
-          <div className="flex flex-col md:flex-row gap-10 items-center max-w-[1200px] mx-auto px-4 md:px-6">
-            <div className="flex-1 flex justify-center order-2 md:order-1">
-              <img
-                src={
-                  backgroundDivIndex === 0
-                    ? "/image/casual.png"
-                    : backgroundDivIndex === 1
-                    ? "/image/casual-1.png"
-                    : backgroundDivIndex === 2
-                    ? "/image/casual-2.png"
-                    : backgroundDivIndex === 3
-                    ? "/image/casual-3.png"
-                    : "/image/casual.png"
-                }
-                alt="casual"
-                className="transition-all duration-300"
-              />
-            </div>
-            <div className="flex-1 flex flex-col md:items-center justify-center order-1 md:order-2">
-              <CasualTimeline
-                {...{ backgroundDivIndex, setBackgroundDivIndex }}
-              />
-              <div className="flex gap-5 mt-10">
-                <button className="w-[40%]">
-                  <img src="/svgs/apple-btn.svg" alt="" />
-                </button>
-                <button className="w-[40%]">
-                  <img src="/svgs/playstore-btn.svg" alt="" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <CasualComponent />
 
       {/* points description */}
-      <div className="bg-black py-5">
-        <div className={`py-20 ${styles.shop}`}>
-          <div className="flex flex-col md:flex-row gap-10 items-center max-w-[1200px] mx-auto px-4 md:px-6">
-            <div className="flex-1 flex justify-center order-2">
-              <img src="/image/points.png" alt="" />
-            </div>
-            <div className="flex-1 flex flex-col md:items-center justify-center order-1">
-              <PointsTimeline />
-              <div className="flex gap-5 mt-10 max-w-[450px]">
-                <button className="w-[40%]">
-                  <img src="/svgs/apple-btn.svg" alt="" />
-                </button>
-                <button className="w-[40%]">
-                  <img src="/svgs/playstore-btn.svg" alt="" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <PointsComponent />
 
       {/* games description */}
       <div className="bg-black py-5 max-w-[1200px] mx-auto px-4 md:px-6 ">
--- src/components/Timeline/PointsTimeline.tsx
+++ src/components/Timeline/PointsTimeline.tsx
@@ -1,39 +1,148 @@
 "use client";
-import React from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import styles from "../../styles/PointsTimeline.module.css";
-import { AnimateIn, useElementOnScreen } from "../AnimateScreen";
+import { AnimateIn } from "../AnimateScreen";
 
-const PointsTimeline = () => {
-  const [activetimeline, setActiveTime] = React.useState(0);
-  const ref = React.useRef<HTMLDivElement>(null);
-  const onScreen = useElementOnScreen(ref);
+type PointTimelineProps = {
+  backgroundDivIndex: number | null;
+  setBackgroundDivIndex: Function;
+};
 
-  const timelineData = [
-    "Experience a Redefined style of Casual Gaming",
-    "Experience a Redefined style of Casual Gaming",
-    "Experience a Redefined style of Casual Gaming",
-    "Experience a Redefined style of Casual Gaming",
-    "Experience a Redefined style of Casual Gaming",
-    "Experience a Redefined style of Casual Gaming",
-  ];
+const PointsTimeline: FC<PointTimelineProps> = ({
+  backgroundDivIndex,
+  setBackgroundDivIndex,
+}) => {
+  const [pointsscrollPosition, setPointsScrollPosition] = useState<number>(4);
+  const divRefs: React.RefObject<HTMLDivElement>[] = [];
 
-  React.useEffect(() => { }, []);
-  
-  console.log(onScreen)
+  const CreateDivRef = (index: number): React.RefObject<HTMLDivElement> => {
+    const ref = useRef<HTMLDivElement>(null);
+    divRefs[index] = ref;
+    return ref;
+  };
+
+  const getTopPositions = () => {
+    const topPositions = divRefs.map((ref) => {
+      if (ref.current) {
+        return ref.current.getBoundingClientRect().top;
+      }
+      return null;
+    });
+    return topPositions;
+  };
+
+  useEffect(() => {
+    // Update the scroll position in the state on scroll
+    const handleScroll = () => {
+      setPointsScrollPosition(window.scrollY);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Get the top positions of each div element
+    const topPositions = getTopPositions();
+
+    // Determine which div should have the background with a slight delay
+    let newIndex: number | null = null;
+    for (let i = 0; i < topPositions.length; i++) {
+      if (topPositions[i] !== null) {
+        const element = divRefs[i].current;
+        if (element) {
+          const isTopInView =
+            // @ts-ignore
+            topPositions[i] - window.innerHeight + element.clientHeight <
+            pointsscrollPosition;
+          // @ts-ignore
+          const isBottomInView = topPositions[i] > pointsscrollPosition;
+          if (isTopInView && isBottomInView) {
+            newIndex = i;
+            break;
+          }
+        }
+      }
+    }
+
+    // Update the background div index state with a slight delay
+    setTimeout(() => {
+      setBackgroundDivIndex((prevIndex: number) =>
+        newIndex !== null ? newIndex : prevIndex
+      );
+    }, 200); // Adjust the delay duration as needed
+  }, [pointsscrollPosition]);
 
   return (
     <div className={`text-white ${styles.timeline_container}`}>
-      {timelineData.map((data, index) => (
-        <AnimateIn
-          className={`min-h-[80px] py-3 pl-10 pr-5 w-[80%] flex items-center ${
-            onScreen ?  styles.timeline_item : ""
-          }`}
-          key={index}
+      <AnimateIn className="">
+        <div
+          ref={CreateDivRef(0)}
+          className={`min-h-[80px] py-3 pl-10 pr-5 w-[80%] flex items-center  ${
+            styles.timeline_item
+          } ${backgroundDivIndex === 0 ? "background_in_view" : ""}`}
         >
-          <p>{data}</p>
-          <span className={`${onScreen ? styles.points_circle : ""}`} />
-        </AnimateIn>
-      ))}
+          <p>Experience a Redefined style of Casual Gaming</p>
+          <span className={`circle ${styles.points_circle}`} />
+        </div>
+      </AnimateIn>
+      <AnimateIn className="">
+        <div
+          ref={CreateDivRef(1)}
+          className={`min-h-[80px] py-3 pl-10 pr-5 w-[80%] flex items-center ${
+            styles.timeline_item
+          } ${backgroundDivIndex === 1 ? "background_in_view" : ""}`}
+        >
+          <p>Experience a Redefined style of Casual Gaming</p>
+          <span className={`circle" ${styles.points_circle}`} />
+        </div>
+      </AnimateIn>
+      <AnimateIn className="">
+        <div
+          ref={CreateDivRef(2)}
+          className={`min-h-[80px] py-3 pl-10 pr-5 w-[80%] flex items-center ${
+            styles.timeline_item
+          } ${backgroundDivIndex === 2 ? "background_in_view" : ""}`}
+        >
+          <p>Experience a Redefined style of Casual Gaming</p>
+          <span className={`circle" ${styles.points_circle}`} />
+        </div>
+      </AnimateIn>
+      <AnimateIn className="">
+        <div
+          ref={CreateDivRef(3)}
+          className={`min-h-[80px] py-3 pl-10 pr-5 w-[80%] flex items-center ${
+            styles.timeline_item
+          } ${backgroundDivIndex === 3 ? "background_in_view" : ""}`}
+        >
+          <p>Experience a Redefined style of Casual Gaming</p>
+          <span className={`circle" ${styles.points_circle}`} />
+        </div>
+      </AnimateIn>
+      <AnimateIn className="">
+        <div
+          ref={CreateDivRef(4)}
+          className={`min-h-[80px] py-3 pl-10 pr-5 w-[80%] flex items-center ${
+            styles.timeline_item
+          } ${backgroundDivIndex === 4 ? "background_in_view" : ""}`}
+        >
+          <p>Experience a Redefined style of Casual Gaming</p>
+          <span className={`circle" ${styles.points_circle}`} />
+        </div>
+      </AnimateIn>
+      <AnimateIn className="">
+        <div
+          ref={CreateDivRef(5)}
+          className={`min-h-[80px] py-3 pl-10 pr-5 w-[80%] flex items-center ${
+            styles.timeline_item
+          } ${backgroundDivIndex === 5 ? "background_in_view" : ""}`}
+        >
+          <p>Experience a Redefined style of Casual Gaming</p>
+          <span className={`circle" ${styles.points_circle}`} />
+        </div>
+      </AnimateIn>
     </div>
   );
 };
