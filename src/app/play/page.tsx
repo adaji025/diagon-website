00480185406e66ef9030922c42143ca087040172
Anibe <samanibe@yahoo.com>
2023-08-05 05:14:44 +0100

    updated games and leadersboard

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,13 +1,11 @@
 "use client"
-import React, { useEffect } from 'react';
-import Head from "next/head";
-import { useSearchParams } from 'next/navigation'
+import React from 'react';
+import Head from 'next/head';
+import { useSearchParams } from 'next/navigation';
 
-
-
-export default function PlayGame() {
-  const searchParams = useSearchParams()
-  const game = searchParams
+const Play = () => {
+  const searchParams = useSearchParams();
+  const game = searchParams.get('game');
 
   return (
     <div>
@@ -19,28 +17,33 @@ export default function PlayGame() {
           content="DIAGON GAMES - A hyper-casual play-to-earn platform that gives you the best digital living experience with crypto."
         />
       </Head>
-      <section
-        id="app-index"
-        className="md:grid grid-cols-12 relative bg-white w-full"
-      >
+      <section id="app-index" className="md:grid grid-cols-12 relative bg-white w-full">
         <div className="app-main overflow-y-scroll md:overflow-y-auto w-full md:grid grid-cols-12 col-span-12">
-          <iframe
-            title="game.title"
-            id="fg-frame-archery-world-tour"
-            width="640"
-            height="640"
-            data-aspect-ratio="1"
-            src={game.url}
-            frameBorder={0}
-            style={{
-              display: 'block',
-              height: '100vh',
-              width: '100vw',
-              zIndex: 1000
-            }}
-          ></iframe>
+          {game && (
+            <iframe
+              title="Game"
+              width="640"
+              height="640"
+              data-aspect-ratio="1"
+              src={game}
+              frameBorder={0}
+              style={{
+                display: 'block',
+                height: '100vh',
+                width: '100vw',
+                zIndex: 1000,
+              }}
+            ></iframe>
+          )}
         </div>
       </section>
     </div>
   );
 };
+
+export default Play;
+
+
+
+
+
