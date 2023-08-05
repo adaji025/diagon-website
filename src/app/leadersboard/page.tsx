ba3a9d9c9089d020e582dcb775d21574ea25ee67
Anibe <samanibe@yahoo.com>
2023-08-05 05:14:38 +0100

    updated games and leadersboard

--- src/app/leadersboard/page.tsx
+++ src/app/leadersboard/page.tsx
@@ -1,23 +1,32 @@
 "use client"
-import TimeFilter from "@/components/TimeFilter/TimeFilter";
 import React, { useState, useEffect } from "react";
-import numeral from "numeral";
+import numeral from 'numeral';
 
-// ... (previous imports)
+interface LeaderData {
+  username: string;
+  value: number;
+  challenge: number;
+  avatar: string;
+  // Add other properties that are present in the leader data
+}
 
-const Leaderboard = () => {
-  const [leaders, setLeaders] = useState([]);
+const LeadersBoard = () => {
+  const [leaders, setLeaders] = useState<LeaderData[]>([]);
 
   useEffect(() => {
     const fetchLeaders = async () => {
       try {
-        const data = await fetch(`https://api.diagon.gg/leaderboard?limit=50`);
-        const leadersData = await data.json();
+        const res = await fetch(`https://api.diagon.gg/leaderboard?limit=50`);
+        const leaders = await res.json();
 
-        const leadersWithNumber = leadersData.map((leader, index) => ({
-          ...leader,
-          number: numeral(index + 1).format("0"),
-        }));
+        // Add number 1 to 50 to each data in the `leaders` array
+        const leadersWithNumber = leaders.map((leader: LeaderData, index: number) => {
+          const number = index + 1;
+          return {
+            ...leader,
+            number: numeral(number).format("0"),
+          };
+        });
 
         setLeaders(leadersWithNumber);
       } catch (error) {
@@ -31,35 +40,52 @@ const Leaderboard = () => {
   return (
     <div className="bg-black text-white pt-[200px]">
       <div className="max-w-[1200px] mx-auto  bg-[#1C1C1E] px-8 md:px-20 pb-[200px]">
-        {/* ... (rest of the component code) */}
-        <tbody>
-          {leaders.map((leader, index) => (
-            <tr key={index}>
-              {/* ... (rest of the table row code) */}
-              <td className="text-right">
-                <div className="flex justify-end">
-                  <div
-                    className={`flex gap-1 justify-center items-center bg-[#292929] rounded-[50px] px-5 py-2 ml-auto max-w-[224px] `}
-                  >
-                    <img src="/svgs/ticket.svg" alt="" />
-                    <div
-                      className={`text-sm sm:text-base ${
-                        leader.value && leader.value > 1000000
-                          ? "text-[#FDD015]"
-                          : "text-white"
-                      }`}
-                    >
-                      {leader.value ? leader.value.toLocaleString() : "N/A"}
+
+        <table className="table-fixed w-full border-separate border-spacing-y-10">
+          <thead className="bg-[#FDD015] text-black">
+            <tr>
+              <th className="py-5 pl-5 sm:pl-10 text-left">CAUSAL GAMERS</th>
+              <th className="py-5 text-right pr-5 sm:pr-10">POINTS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {leaders.map((leader, index) => (
+              <tr key={index}>
+                <td>
+                  <div className="flex items-center gap-5 sm:gap-10">
+                    {index + 1}
+                    <div className="flex h-[60px] sm:h-[80px] w-[60px] sm:w-[80px] rounded-full relative border-2 border-white/20">
+                      <img
+                        className="rounded-full"
+                        src={
+                          "/images/avatars/" + leader.avatar + ".png"
+                        }
+
+                        alt={leader.avatar}
+                      />
+                      <div className="absolute bottom-0 right-0 h-[20px] sm:h-[30px] w-[20px] sm:w-[30px] rounded-full border-2 border-[#0F0F0F] bg-[#00C04E] flex justify-center items-center z-10">
+                        <img src="/svgs/award.svg" alt="" />
+                      </div>
+                    </div>
+
+                    {leader.username}
+                  </div>
+                </td>
+                <td className="text-right">
+                  <div className="flex justify-end">
+                    <div className={`flex gap-1 justify-center items-center bg-[#292929] rounded-[50px] px-5 py-2 ml-auto max-w-[224px] `}>
+                      <img src="/svgs/ticket.svg" alt="" />
+                      <div className="text-sm sm:text-base  text-[#FDD015]">  {numeral(leader.challenge).format("0,0")}</div>
                     </div>
                   </div>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 };
 
-export default Leaderboard;
+export default LeadersBoard;
