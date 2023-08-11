81fb159ef508fd3e791ff4f83c0cb23be76d69b0
Anibe Samuel <samanibe@yahoo.com>
2023-08-11 15:26:11 +0100

    Update page.jsx

--- src/app/privacy-policy/page.jsx
+++ src/app/privacy-policy/page.jsx
@@ -1,6 +1,6 @@
 import Policy from "@/components/PrivacyPolicy/PrivacyPolicy";
 
-export default function Privicypolicypage() {
+export default function Policy() {
   return (
     <div className="bg-black">
       <Policy />
